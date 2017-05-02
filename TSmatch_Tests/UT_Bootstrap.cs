62e886e5ba036885305b7852561c1831dbcada21
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-05-02 03:03:31 +0300

    WPF branch tests
    Unit Test'ы SavedReport и Bootstrap в зачаточном состоянии

---
+++ TSmatch_Tests/UT_Bootstrap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSmatch.Bootstrap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FileOp = match.FileOp.FileOp;
+
+namespace TSmatch.Bootstrap.Tests
+{
+    [TestClass()]
+    public class BootstrapTests
+    {
+        [TestMethod()]
+        public void UT_Bootstrap()
+        {
+            var boot = new Bootstrap();
+
+            Assert.IsTrue(boot.ModelDir.Length > 0);
+            Assert.IsTrue(boot.TOCdir.Length > 0);
+            Assert.IsNotNull(boot.docTSmatch);
+            Assert.AreEqual(boot.docTSmatch.name, "TOC");
+            Assert.IsTrue(boot.docTSmatch.il > 20);
+
+            FileOp.AppQuit();
+        }
+    }
+}
---
+++ TSmatch_Tests/UT_SavedReport.cs
@@ -0,0 +1,44 @@
+﻿/*=================================
+ *  Saved Report Unit Test 1.5.2017
+ *=================================
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSmatch.SaveReport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Decl = TSmatch.Declaration.Declaration;
+using Docs = TSmatch.Document.Document;
+
+namespace TSmatch.SaveReport.Tests
+{
+    [TestClass()]
+    public class SavedReportTests
+    {
+        [TestMethod()]
+        public void UT_getSavedReport()
+        {
+            var boot = new Bootstrap.Bootstrap();
+            var sr = new SavedReport();
+            //1/5            model = sr;
+            //1/5            model.SetModel();
+            sr.getSavedReport();
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void UT_Recover()
+        {
+            var boot = new Bootstrap.Bootstrap();
+            var sr = new SavedReport();
+
+            string repNm = Decl.TSMATCHINFO_MODELINFO;
+            sr.Recover(repNm, SavedReport.RecoverToDo.ResetRep);
+            Assert.IsTrue(Docs.IsDocExists(repNm));
+
+            Docs modINFO = Docs.getDoc(Decl.TSMATCHINFO_MODELINFO);
+        }
+    }
+}
