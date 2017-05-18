c8812fd7d29ae1790c5322d01014b9d92ef37b24
Павел Храпкин <Khrapkin.P@csitltd.ru>
2017-05-18 17:17:12 +0300

    Material and Profile Window add

---
+++ TSmatch/WindowMaterialChoice.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TSmatch
+{
+    /// <summary>
+    /// Interaction logic for WindowMaterialChoice.xaml
+    /// </summary>
+    public partial class WindowMaterialChoice : Page
+    {
+        public WindowMaterialChoice()
+        {
+            InitializeComponent();
+            Title = "TSmatch: выбор материала";
+//18/5            List<Supl> items = new List<Supl>();
+        }
+
+        private void OK_button_Click(object sender, RoutedEventArgs e)
+        {
+            //Close();
+        }
+    }
+}
