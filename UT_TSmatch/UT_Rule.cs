3229deebea40ed9225329cf8186876d03bb53dec
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-06-06 02:38:13 +0300

    cosmetic

---
+++ UT_TSmatch/UT_Rule.cs
@@ -0,0 +1,24 @@
+﻿/*=================================
+ * Rule Unit Test 4.6.2017
+ *=================================
+ */
+ using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSmatch.Rule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSmatch.Rule.Tests
+{
+    [TestClass()]
+    public class UT_Rule
+    {
+        [TestMethod()]
+        public void UT_RuleSynParse()
+        {
+            Assert.Fail();
+        }
+    }
+}
