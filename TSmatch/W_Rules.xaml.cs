f0d17ef4fab8d8913e5ac7dfcdab47dc4d1aba85
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-08-07 22:48:51 +0300

    W_Rules transfer from PavelDELL

--- TSmatch/W_Rules.xaml.cs
+++ TSmatch/W_Rules.xaml.cs
@@ -15,6 +15,7 @@ using log4net;
 using Lib = match.Lib.MatchLib;
 using Decl = TSmatch.Declaration.Declaration;
 using Docs = TSmatch.Document.Document;
+using System.Linq;
 
 namespace TSmatch
 {
@@ -33,13 +34,21 @@ namespace TSmatch
             Title = "TSmatch: работа с правилами";
             List<Rl> items = new List<Rl>();
             Docs doc = Docs.getDoc(Decl.TSMATCHINFO_RULES);
+
             for (int i = doc.i0; i <= doc.il; i++)
             {
                 string csName = doc.Body.Strng(i, Decl.RULE_COMPSETNAME);
                 Rule.Rule rule = new Rule.Rule(i);
-                if (rule == null || string.IsNullOrWhiteSpace(rule.Supplier.name)
+                if (rule == null || string.IsNullOrWhiteSpace(rule.sSupl)
                     || string.IsNullOrWhiteSpace(rule.text)) continue;
-                items.Add(new Rl(rule.date, rule.Supplier.name, csName, rule.text));
+                int nElms = 0;
+                foreach (var mtch in MainWindow.model.matches)
+                {
+                    if (mtch.rule.sSupl != rule.sSupl || mtch.rule.sCS != rule.sCS) continue;
+                    var gr = mtch.group;
+                    nElms += gr.guids.Count;
+                }
+                items.Add(new Rl(nElms, rule.date, rule.sSupl, csName, rule.text));
                 rules.Add(rule);
             }
             WRules.ItemsSource = items;
@@ -62,14 +71,16 @@ namespace TSmatch
 
         private class Rl : IComparable<Rl>
         {
+            public string nElms { get; set; }
             public string Date { get; set; }
             public string Supplier { get; set; }
             public string CompSet { get; set; }
             public string RuleText { get; set; }
             public bool Flag { get; set; }
 
-            public Rl(DateTime date, string supl, string cs, string ruletxt)
+            public Rl(int _nElms, DateTime date, string supl, string cs, string ruletxt)
             {
+                nElms = _nElms.ToString();
                 Date = date.ToString("d.MM.yy H:mm");
                 Supplier = supl;
                 CompSet = cs;
