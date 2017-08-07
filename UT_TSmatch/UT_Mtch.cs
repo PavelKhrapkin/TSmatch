8396f3867a7fc64499d4d9c17126746d5ce26295
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-08-07 23:28:01 +0300

    UT_Match from PavelDELL

--- UT_TSmatch/UT_Mtch.cs
+++ UT_TSmatch/UT_Mtch.cs
@@ -1,14 +1,16 @@
 ﻿/*=================================
-* Match Unit Test 21.7.2017
+* Match Unit Test 4.8.2017
 *=================================
 */
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 using FileOp = match.FileOp.FileOp;
 using Boot = TSmatch.Bootstrap.Bootstrap;
 using Mod = TSmatch.Model.Model;
 using SR = TSmatch.SaveReport.SavedReport;
 using MH = TSmatch.Handler.Handler;
+using Elm = TSmatch.ElmAttSet.ElmAttSet;
 
 namespace TSmatch.Matcher.Tests
 {
@@ -20,23 +22,46 @@ namespace TSmatch.Matcher.Tests
         {
             var boot = new Boot();
             var model = new Mod();
-            var sr = model.sr = new SR();
-            model.mh = new MH();
-            model.SetModDir(boot);
-//23/7            model.dir = model.sr.dir;
-//23/7            model.name = sr.name;
-            var elements = model.elements = sr.Raw(model);
-            model.elmGroups = model.mh.getGrps(model.elements);
-            model.sr.getSavedRules();
+            //4/8.2017            model.SetModel(boot);
+            //7/8            model.SetModDir(boot);
+            var sr = new SR();
+            model.elements = sr.Raw(model);
+            List<Elm> elmCopy = new List<Elm>();
+            foreach (Elm elm in model.elements) elmCopy.Add(elm);
+            for (int i = 0; i < elmCopy.Count; i++) Assert.AreEqual(elmCopy[i], model.elements[i]);
+            int cnt = model.elements.Count;
+            string MD5 = model.getMD5(model.elements);
+            Assert.IsTrue(cnt > 0);
+            string cMD5 = model.getMD5(elmCopy);
+            Assert.AreEqual(cMD5, MD5);
+            var mh = new MH();
+            model.elmGroups = mh.getGrps(model.elements);
+            Assert.IsTrue(model.elmGroups.Count > 0);
 
-            string md5 = model.getMD5(model.elements);
+            model = sr.GetSavedRules(model, init: true);
+            Assert.IsTrue(model.Rules.Count > 0);
 
-            var gr = model.elmGroups[0];
-            foreach (var rule in model.Rules)
+            foreach (var gr in model.elmGroups)
             {
-                Mtch _match = new Mtch(gr, rule);
-                string new_md5 = model.getMD5(model.elements);
-                Assert.AreEqual(new_md5, md5);
+                //4/8                var gr = model.elmGroups[0];
+                Assert.IsTrue(model.Rules.Count > 0);
+
+                var mtch = new Mtch(model);
+
+                foreach (var rule in model.Rules)
+                {
+                    Assert.IsNotNull(rule.CompSet.Supplier);
+                    Assert.IsTrue(rule.CompSet.Components.Count > 0);
+
+                    Assert.IsTrue(mtch.OK_MD5());
+
+                    Mtch _match = new Mtch(gr, rule);
+
+                    Assert.IsTrue(mtch.OK_MD5());
+
+                    string new_md5 = model.getMD5(model.elements);
+                    Assert.AreEqual(new_md5, MD5);
+                }
             }
 
             FileOp.AppQuit();
