d6d163cd7eed515803e025a33edad8e8b1223c27
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-08-19 12:09:59 +0300

    ModelWrFile string format used

--- TSmatch/ModelWrFile.cs
+++ TSmatch/ModelWrFile.cs
@@ -1,13 +1,15 @@
 ﻿/*--------------------------------------------------------------------------------------------
  * ModelWrFile : Model -- Write ModelINFO, Report and other documents in file TSmatchINFO.xlsx
  *
- *  19.07.2017 Pavel Khrapkin
+ *  18.08.2017 Pavel Khrapkin
  *
- *--- History ---
- *  13.07.2017 taken from Model code
- *  19.07.2017 Lib.timeStr(date) instead of DateTime format- it is strange behavior of Excel fix
  *--- Unit Tests --- 
- * 2017.06.19 UT_ModHandler.UT_Hndl, UT_Pricing OK
+ * 2017.08.9 UT_ModelWrFile: UT_sInt, UT_iDbl, UT_sDat OK
+ *--- History ---
+ * 13.07.2017 taken from Model code
+ * 19.07.2017 Lib.timeStr(date) instead of DateTime format- it is strange behavior of Excel fix
+ *  9.08.2017 Docs.getDoc(reset:true); use sInt, sDbl, sDat
+ * 18.08.2017 Write to Report gr.compDescription string, delete isMatches method
  * -------------------------------------------------------------------------------------------
  * Methods:
  * wrModel(mode, model) - write model in ModelINFO, Report, and other tabs of TSmatchINFO.xlsx
@@ -19,9 +21,8 @@ using Decl = TSmatch.Declaration.Declaration;
 using Log = match.Lib.Log;
 using Lib = match.Lib.MatchLib;
 using Docs = TSmatch.Document.Document;
-using Mtch = TSmatch.Matcher.Mtch;
-using SType = TSmatch.Section.Section.SType;
 using Mod = TSmatch.Model.Model;
+using Msg = TSmatch.Message.Message;
 
 namespace TSmatch.Model.WrModelInfo
 {
@@ -29,24 +30,13 @@ namespace TSmatch.Model.WrModelInfo
     {
         public static readonly ILog log = LogManager.GetLogger("ModelWrFile");
 
-        /// <summary>
-        /// wrModel(doc_name) - write formatted data from mod to Excel file
-        /// </summary>
-        /// <param name="doc_name">document to be written name</param>
-        /// <history>16.3.2016
-        /// 18.3.2016 - write in Excel list of Rules in FORM_RULE
-        /// 26.3.2016 - use rule.CompSet.name reference instead of doc.name
-        ///  1.4.2016 - re-written
-        /// 21.8.2016 - case constants defined here from Decl, changed TSmatchINFO Document list, restructured
-        /// 10.4.2017 - enum WrMod
-        /// </history>
         public enum WrMod { ModelINFO, Materials, Suppliers, Rules, Report }
         public void wrModel(WrMod mode, Mod mod)
         {
             string doc_name = mode.ToString();
             Log.set("Model.wrModel(" + doc_name + ")");
             DateTime t0 = DateTime.Now;
-            Docs doc = Docs.getDoc(doc_name, create_if_notexist: true);
+            Docs doc = Docs.getDoc(doc_name, create_if_notexist: true, reset: true);
             doc.Reset();
             switch (mode)
             {
@@ -55,8 +45,8 @@ namespace TSmatch.Model.WrModelInfo
                     string adr = mod.adrCity;
                     if (mod.adrStreet != string.Empty) adr += ", " + mod.adrStreet;
                     doc.wrDocForm(mod.name, adr, mod.dir, mod.phase
-                        , Lib.timeStr(mod.date), mod.MD5
-                        , mod.elements.Count, Lib.timeStr(mod.pricingDate), mod.pricingMD5);
+                        , sDat(mod.date), mod.MD5
+                        , sInt(mod.elements.Count), sDat(mod.pricingDate), mod.pricingMD5);
                     break;
                 case WrMod.Materials:   // сводка по материалам, их типам (бетон, сталь и др)
                     doc.wrDocSetForm("FORM_Materials", 3, AutoFit: true);
@@ -73,10 +63,11 @@ namespace TSmatch.Model.WrModelInfo
                     }
                     break;
                 case WrMod.Rules:       // перечень Правил, используемых для обработки модели
+                    if (mod.Rules.Count == 0) Msg.F("Can't write TSmatchINFO.xlsx/Rules");
                     doc.wrDocSetForm("FORM_RuleLine");
                     foreach (var rule in mod.Rules)
                     {
-                        doc.wrDocForm(rule.date, rule.Supplier.name, rule.CompSet.name, rule.text);
+                        doc.wrDocForm(sDat(rule.date), rule.sSupl, rule.sCS, rule.text);
                     }
                     break;
                 case WrMod.Report:      // отчет по сопоставлению групп <материал, профиль> c прайс-листами поставщиков
@@ -84,17 +75,17 @@ namespace TSmatch.Model.WrModelInfo
                     int n = 1;
                     foreach (var gr in mod.elmGroups)
                     {
-                        string foundDescr = "", suplName = "", csName = "";
-                        if (gr.match != null && gr.match.ok == Mtch.OK.Match)
+                        string compDescription = "", suplName = "", csName = "";
+                        if (!string.IsNullOrEmpty(gr.SupplierName))
                         {
-                            foundDescr = gr.match.component.Str(SType.Description);
-                            suplName = gr.match.rule.Supplier.name;
-                            csName = gr.match.rule.CompSet.name;
+                            suplName = gr.SupplierName;
+                            csName = gr.CompSetName;
+                            compDescription = gr.compDescription;
                         }
                         doc.wrDocForm(n++, gr.Mat, gr.Prf
-                            , gr.totalLength, gr.totalWeight, gr.totalVolume
-                            , foundDescr, suplName, csName
-                            , gr.totalWeight, gr.totalPrice);
+                            , sDbl00(gr.totalLength), sDbl(gr.totalWeight), sDbl(gr.totalVolume)
+                            , compDescription, suplName, csName
+                            , sDbl(gr.totalWeight), sDbl(gr.totalPrice));
                     }
                     doc.isChanged = true;
                     doc.saveDoc();
@@ -117,5 +108,10 @@ namespace TSmatch.Model.WrModelInfo
             log.Info("Время записи в файл \"" + doc_name + "\"\t t= " + (DateTime.Now - t0).ToString() + " сек");
             Log.exit();
         }
+
+        public string sDbl(double v) { return string.Format("{0 :N2}", v); }
+        public string sInt(int i) { return string.Format("{0 :N0}", i); }
+        public string sDbl00(double v) { return string.Format("{0 :N0}", v); }
+        public string sDat(DateTime d) { return Lib.timeStr(d, "d.MM.yyyy H:mm"); }
     } // end class
 } // end namespace
