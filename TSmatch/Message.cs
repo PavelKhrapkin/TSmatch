6593df2edd72fa9725834e3afc84e5e228283d7a
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-11-03 23:40:07 +0300

    Message updated

--- TSmatch/Message.cs
+++ TSmatch/Message.cs
@@ -1,10 +1,10 @@
 ﻿/*----------------------------------------------------------------------------
  * Message -- multilanguage message system
  * 
- * 8.10.2017  Pavel Khrapkin
+ * 3.11.2017  Pavel Khrapkin
  *
  *--- Unit Tests ---
- * UT_Message: UT_Init, UT_AskS, UT_W, UT_S 13.09.2017 OK
+ * UT_Message: UT_Init, UT_AskS, UT_W, UT_S, UT_AskOK, UT_AskYN 25.09.2017 OK
  * --- History ---
  * Feb-2016 Created
  * 20.3.2016 - Error message Code display even when Message system is not initialysed yet
@@ -17,6 +17,8 @@
  * 12.9.2017 - TSmatchMsg.resx and TSmatchMsg,ru.resx use as localization resources
  * 14.9.2017 - static constructor as a singleton for _messages fill; ArgumentException for UTs
  * 8.10.2017 - Instance F,W,I istead of static
+ * 25.10.2017 - Exception instead of MessageBox in AskOK. AskYN for UT
+ *  3.11.2017 - set Language from Property.Setting or from CultureInfo.CurrentCulture
  * ---------------------------------------------------------------------------------------
  *      Methods:
  * static Message() - Singleton constructor initiate static msgs Dictionary set
@@ -28,14 +30,12 @@
  * AskYN(text?)  - ask with text as a prompt, and respont Yes or No
  * AskS(text:)   - request string entry with text as a prompt
  */
+using log4net;
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Globalization;
 using System.Resources;
-using System.Threading;
-using log4net;
-
+using System.Windows;
 using FileOp = match.FileOp.FileOp;
 
 namespace TSmatch.Message
@@ -58,26 +58,39 @@ namespace TSmatch.Message
         protected static ResourceManager mgr = Properties.TSmatchMsg.ResourceManager;
 
         /// <summary>
-        /// singleton Message system initialization -- ToDo 31.8.17 make it with rsx Localization
+        /// singleton Message system initialization
+        /// 
+        /// <para>
+        /// Culture stored in Property.Setting.Default.sLanguage
+        /// </para>
         /// </summary>
         static Message()
         {
-            ResourceSet set = mgr.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+            string sLang = Properties.Settings.Default.sLanguage;
+            if (string.IsNullOrWhiteSpace(sLang))
+            {
+                sLang = CultureInfo.CurrentCulture.Name;
+                Properties.Settings.Default.sLanguage = sLang;
+                Properties.Settings.Default.Save();
+            }
+            CultureInfo culture = CultureInfo.GetCultureInfo(sLang);
+
+            ResourceSet set = mgr.GetResourceSet(culture, true, true);
             foreach (System.Collections.DictionaryEntry o in set)
             {
                 _messages.Add(o.Key as string, o.Value as string);
             }
             mgr.ReleaseAllResources();
         }
-  
+
         protected static string msg, errType;
-        static void txt(Severity type, string msgcode, object[] p, bool doMsgBox=true)
+        static void txt(Severity type, string msgcode, object[] p, bool doMsgBox = true)
         {
             msgcode = msgcode.Replace(' ', '_');
             errType = "TSmatch " + type;
             bool knownMsg = _messages.ContainsKey(msgcode);
             try { msg = knownMsg ? string.Format(_messages[msgcode], p) : string.Format(msgcode, p); }
-            catch { msg = knownMsg? _messages[msgcode]: msgcode; errType = "(!)" + errType; }
+            catch { msg = knownMsg ? _messages[msgcode] : msgcode; errType = "(!)" + errType; }
             if (!knownMsg) errType = "(*)" + errType;
             if (!Dialog) throw new ArgumentException("Msg.F");
             if (doMsgBox) MessageBox.Show(msg, errType, MessageBoxButton.OK, MessageBoxImage.Asterisk, reply, MessageBoxOptions.ServiceNotification);
@@ -85,9 +98,9 @@ namespace TSmatch.Message
         }
 
         public static void txt(string str, params object[] p) { txt(Severity.INFO, str, p, doMsgBox: false); }
-        public static void F(string str, params object[] p)   { txt(Severity.FATAL, str, p); }
-        public static void W(string str, params object[] p)   { txt(Severity.WARNING, str, p); }
-        public static void I(string str, params object[] p)   { txt(Severity.INFO, str, p); }
+        public static void F(string str, params object[] p) { txt(Severity.FATAL, str, p); }
+        public static void W(string str, params object[] p) { txt(Severity.WARNING, str, p); }
+        public static void I(string str, params object[] p) { txt(Severity.INFO, str, p); }
         public static string S(string str, params object[] p)
         {
             txt(Severity.SPLASH, str, p, doMsgBox: false);
@@ -95,7 +108,8 @@ namespace TSmatch.Message
             return msg;
         }
         //--new no static
-        protected bool DDialog=true;
+        private bool dDialog;
+        public bool DDialog { get => dDialog; set => dDialog = value; }
 
         protected string mmsg, eerrType;
         protected void ttxt(Severity type, string msgcode, object[] p, bool doMsgBox = true)
@@ -106,7 +120,8 @@ namespace TSmatch.Message
             try { mmsg = knownMsg ? string.Format(_messages[msgcode], p) : string.Format(msgcode, p); }
             catch { mmsg = knownMsg ? _messages[msgcode] : msgcode; eerrType = "(!)" + eerrType; }
             if (!knownMsg) eerrType = "(*)" + eerrType;
-            if (!DDialog && type == Severity.FATAL) throw new ArgumentException("Msg.F");
+            // PKh> To have a look to the MessageBox with the message, add comment to the next line check Dialog
+            if (!DDialog && type == Severity.FATAL) throw new ArgumentException("Msg.F: " + mmsg);
             if (doMsgBox) MessageBox.Show(mmsg, eerrType, MessageBoxButton.OK, MessageBoxImage.Asterisk, reply, MessageBoxOptions.ServiceNotification);
             if (type == Severity.FATAL) Stop();
         }
@@ -121,7 +136,34 @@ namespace TSmatch.Message
             if (eerrType.Contains("(")) mmsg = eerrType + " " + mmsg;
             return mmsg;
         }
-//--new no static
+
+        public bool AAskYN(string msgcode, params object[] p)
+        {
+            ttxt(Severity.INFO, msgcode, p, doMsgBox: false);
+            // PKh> To have a look to the MessageBox with the message, add comment to the next line check Dialog
+            if (!DDialog) throw new ArgumentException("Msg.AskYN: " + mmsg);
+            var X = MessageBox.Show(msg, eerrType, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return X == MessageBoxResult.Yes;
+        }
+
+        private MessageBoxResult rreply;
+
+        public void AAskOK(string msgcode, params object[] p)
+        {
+            ttxt(Severity.INFO, msgcode, p, doMsgBox: false);
+            // PKh> To have a look to the MessageBox with the message, add comment to the next line check Dialog
+            if (!DDialog) throw new ArgumentException("Msg.AskOK: " + mmsg);
+            rreply = MessageBox.Show(mmsg, errType, MessageBoxButton.OKCancel, MessageBoxImage.Question);
+        }
+
+        public void AAskFOK(string msgcode, params object[] p)
+        {
+            AAskOK(msgcode, p);
+            if (rreply == MessageBoxResult.OK) return;
+            Stop();
+        }
+        //--new no static
+
         public static bool AskYN(string msgcode, params object[] p)
         {
             txt(Severity.INFO, msgcode, p, doMsgBox: false);
@@ -147,8 +189,8 @@ namespace TSmatch.Message
         public static string AskS(string msgcode, params object[] p)
         {   // NotImplemetedYet !!
             txt(Severity.INFO, msgcode, p, doMsgBox: false);
- //           string str = string.Empty;
-   //         str = MessageBox.Show(msg);
+            //           string str = string.Empty;
+            //         str = MessageBox.Show(msg);
             return null;
         }
         public static void Stop()
